cf18b624fbd28d5fcfbb8f3a8ec057af6b9ccea6
Krzysztof Rasala <rasala.krzysztof91@gmail.com>
2026-08-13 08:22:18 +0200

    fix: Self-healing database auto-migration on Django startup if django_session table is missing

--- movies/apps.py
+++ movies/apps.py
@@ -3,3 +3,14 @@ from django.apps import AppConfig
 class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies'
+
+    def ready(self):
+        try:
+            from django.core.management import call_command
+            from django.db import connection
+            tables = connection.introspection.table_names()
+            if 'django_session' not in tables:
+                call_command('migrate', interactive=False)
+        except Exception:
+            pass
+
